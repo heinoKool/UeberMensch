819896c003a9f961b92690f5b78bab669f2a8ee4
Van-Lang <pham@bsinfo.eu>
2025-10-30 01:10:41 +0100

    added solution

--- main/src/UeberMensch.java
+++ main/src/UeberMensch.java
@@ -7,7 +7,46 @@ public class UeberMensch {
     int healthPoints;
 
     // Add constructors here
+    // Default constructor (automatisch vorhanden)
+    // verschwindet, wenn andere Konstuktoren hinzugefügt werden
+    UeberMensch() {
+    }
 
+    // Aufgabe 1
+    // this keyword für Zugriff auf Instanz-/Objekt-Variablen
+    UeberMensch(String name, String lastName, String userName, String birthday, int strength, int healthPoints) {
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.birthday = birthday;
+        this.strength = strength;
+        this.healthPoints = healthPoints;
+    }
+
+    // Aufgabe 2
+    // `this` keyword für Zugriff auf Instanz-/Objekt-Variablen
+    UeberMensch(String name, String lastName, String userName, String birthday) {
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.birthday = birthday;
+        this.strength = 50;
+        this.healthPoints = 50;
+    }
+
+    // Parameter können vertauscht werden, falls Reihenfolge der datentypen sich ändert
+    UeberMensch(int strength, int healthPoints, String name, String lastName, String userName, String birthday) {
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.birthday = birthday;
+        this.strength = 50;
+        this.healthPoints = 50;
+    }
+
+    public String getName() {
+        return name;
+    }
     void shout() {
         System.out.println("Hello I am " + userName + " an UeberMensch");
     }
